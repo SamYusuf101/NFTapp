fd5d21c52f9281fd761bd6eab7b173f1921a75db
zilla <samyusuf101@gmail.com>
2022-04-20 04:34:45 -0700

    created Screen pages

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme, 
+  colors: {
+    ...DefaultTheme.colors,
+    background: "Transparent"
+  }
+}
+
+const App = () => {
   return (
-    <View >
-      <Text>Open your pkppzzzzzzzzzzkkp!</Text>
-      
-    </View>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator screenOptions={{hearderShown:'false'}} initialRouteName="Home">
+        <Stack.Screen name= "Home" component={Home}/>
+        <Stack.Screen name= "Details" component={Details}/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
 
 
