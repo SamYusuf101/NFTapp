6d5dba245f653e010ef240b638abe1e611f2f1ae
zilla <samyusuf101@gmail.com>
2022-04-20 05:44:17 -0700

    updated app.js

--- App.js
+++ App.js
@@ -1,24 +1,39 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { StyleSheet, Text, View, } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useFonts, Usefonts } from 'expo-font';
+import Home from './Screens/Home';
+import Details from './Screens/Details';
 
 const Stack = createStackNavigator();
 
 const theme = {
-  ...DefaultTheme, 
+  ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    background: "Transparent"
+    background: "white"
   }
 }
 
 const App = () => {
+
+  // const [loaded] = useFonts({
+    
+  //   InterSemiBold: require ("./assets/fonts/Inter-SemiBold.ttf"),
+  //   InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+  //   InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+  //   InterLight: require("./assets/fonts/Inter-Light.ttf"),
+
+  // });
+
+  // if (!loaded) return null;
+
+
   return (
     <NavigationContainer theme={theme}>
-      <Stack.Navigator screenOptions={{hearderShown:'false'}} initialRouteName="Home">
-        <Stack.Screen name= "Home" component={Home}/>
-        <Stack.Screen name= "Details" component={Details}/>
+      <Stack.Navigator screenOptions={{ hearderShown: 'false' }} initialRouteName="Home">
+        <Stack.Screen options={{ headerShown: false }} name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
 
       </Stack.Navigator>
     </NavigationContainer>
---
+++ Screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { SafeAreaFrameContext, SafeAreaView } from 'react-native-safe-area-context';
+
+const Home = () => {
+  return (
+      <SafeAreaView>
+    <Text>Home</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home;
